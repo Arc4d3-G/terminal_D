179c85b72568e92bc6a66e4dce2efdbf8a07d22d
Dewald Breed <brddew8@protonmail.com>
2024-12-26 13:27:52 +0200

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/terminalD/', // Set the base path for assets
+  build: {
+    outDir: 'dist', // Ensure output is to 'dist'
+  },
 });
