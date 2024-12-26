977d125a8cdadbae4c9000e8f3a19b71b22150db
Dewald Breed <brddew8@protonmail.com>
2024-12-26 15:25:15 +0200

    reverted back to pre-submodule state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import './App.css';
 import Verify from './pages/Verify';
 import TerminalD from './pages/TerminalD';
-import { Routes, Route } from 'react-router-dom';
-console.log(import.meta.env.VITE_BACKEND_URL); // Should output the URL like 'https://dewaldbreed.co.za/api'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <Routes>
-      <Route
-        path='/verify'
-        element={<Verify />}
-      />
-      <Route
-        path='/'
-        element={<TerminalD />}
-      />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/verify'
+          element={<Verify />}
+        />
+        <Route
+          path='/'
+          element={<TerminalD />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/terminalD/', // Set the base path for assets
-  build: {
-    outDir: 'dist', // Ensure output is to 'dist'
-  },
-  define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(process.env.VITE_BACKEND_URL),
-  },
 });
