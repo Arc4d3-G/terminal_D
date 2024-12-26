8f199c2ddfaf575352f68276e54935a827b88079
Dewald Breed <brddew8@protonmail.com>
2024-12-26 14:24:22 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import Verify from './pages/Verify';
 import TerminalD from './pages/TerminalD';
 import { Routes, Route } from 'react-router-dom';
-
+console.log(import.meta.env.VITE_BACKEND_URL); // Should output the URL like 'https://dewaldbreed.co.za/api'
 function App() {
   return (
     <Routes>
