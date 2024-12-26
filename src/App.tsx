1246eccb1d1f031f3c3d52f69d1c48bcffc29d56
Dewald Breed <brddew8@protonmail.com>
2024-12-26 12:53:05 +0200

    routes fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
 import './App.css';
 import Verify from './pages/Verify';
 import TerminalD from './pages/TerminalD';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename='/terminalD'>
-      <Routes>
-        <Route
-          path='/verify'
-          element={<Verify />}
-        />
-        <Route
-          path='/'
-          element={<TerminalD />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path='/verify'
+        element={<Verify />}
+      />
+      <Route
+        path='/'
+        element={<TerminalD />}
+      />
+    </Routes>
   );
 }
 
