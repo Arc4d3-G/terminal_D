37c6ac04db377b7514257967ea13fc40bad770aa
Dewald Breed <brddew8@protonmail.com>
2024-12-26 15:31:56 +0200

    route update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename='/terminalD'>
       <Routes>
         <Route
           path='/verify'
