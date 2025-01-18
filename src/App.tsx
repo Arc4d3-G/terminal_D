f5c4f146972dc825dfd6ff04d3215e8fc44b9bc8
Dewald Breed <brddew8@protonmail.com>
2025-01-18 11:34:53 +0200

    Moved verification logic to be server side

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css';
-import Verify from './pages/Verify';
 import TerminalD from './pages/TerminalD';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
@@ -7,10 +6,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path='/verify'
-          element={<Verify />}
-        />
         <Route
           path='/'
           element={<TerminalD />}
